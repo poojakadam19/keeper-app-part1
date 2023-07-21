d129da7d1a7fcbc99c9f5b40968f8aaa234ca570
Pooja kadam <pkadam9503@gmail.com>
2023-07-21 18:00:28 +0530

    KeeperAPPPart1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {header} from './Header';
+import App from './App';
+//import styles from '../public/styles';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+<App></App>
+);
+
+
